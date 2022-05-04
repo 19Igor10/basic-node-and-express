3adc78556954516c5044af6dd9786735937ecc74
IgorP <igorprzybysz999@gmail.com>
2022-05-04 18:22:33 +0200

    Exercises FreeCodeCamp

--- myApp.js
+++ myApp.js
@@ -1,9 +1,29 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
+// console.log("Hello World")
+
+app.get("/", function(req, res) {
+  res.sendFile( __dirname + "/views/index.html");
+});
 
 
+app.use("/public", express.static(__dirname + "/public"))
 
 
+// app.get("/json", function(req,res) {
+//   res.json({"message": "Hello json"})
+// })
+
+app.get("/json" , function(req,res) {
+  if(process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"})
+  }
+  else {
+    res.json({"message": "Hello json"})
+  }
+});
 
 
 
